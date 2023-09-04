2cf65860a61b70112c93456eea113c2fd391dd19
nigelseah24 <nigel.seah@gmail.com>
2023-09-04 18:13:40 +0800

    Updated gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,7 +6,7 @@ export default function Gallery(props) {
             {/* <a data-flickr-embed="true" href={props.item.href} title={props.item.title} target='_blank'> */}
                 <img src={props.item.imgSrc} alt={props.item.title}/>
             {/* </a> */}
-            {/* <script async src={props.item.scriptSrc} charset="utf-8"></script>            */}
+            {/* <script async src={props.item.scriptSrc} charset="utf-8"></script> */}
         </div>
     )   
 }
