f28d256c720758d711366ee70dc0f64b5c0d9a27
Nigel Seah <114347598+nigelseah24@users.noreply.github.com>
2023-10-23 22:59:23 +0100

    Added contact me email feature

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,82 +1,36 @@
 import { useState } from 'react';
 import '../App.css';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: '',
-  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // You can add your email sending logic here
-    console.log('Form submitted with data:', formData);
-    // Reset the form after submission
-    setFormData({
-      name: '',
-      email: '',
-      subject: '',
-      message: '',
-    });
+  const sendEmail = (e) => {
+    e.preventDefault(); // prevents the page from reloading when you hit “Send”
+ 
+    emailjs.sendForm('service_37w2npe', 'template_38q34pc', e.target, 'H8HrROEjeg5nJCqK4')
+      .then((result) => {
+          // show the user a success message
+          console.log(result.text);
+      }, (error) => {
+          // show the user an error
+          console.log(error.text);
+      });
+      e.target.reset();
   };
 
   return (
-    <div className="contact-form">
-      <h2>Contact Me</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="subject">Subject</label>
-          <input
-            type="text"
-            id="subject"
-            name="subject"
-            value={formData.subject}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            rows="4"
-            required
-          ></textarea>
-        </div>
-        <button type="submit">Send</button>
+      <form class="contact-me-form" onSubmit={sendEmail}>
+        <h2 class="contact-me-form-title">Contact Me</h2>
+        <input type="name" placeholder="Name" name="name" />
+        <input type="email" placeholder="Email" name="email" />
+        <input type="subject" placeholder="Subject" name="subject" />
+        <textarea placeholder="Message" name="message"></textarea>
+        <button class="button-confirm" type="submit">Let`s go → </button>
       </form>
-    </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ export default function Header({ toggleName, toggleShowGallery, toggleShowAbout,
     return (
         <div className="header">
             <h1 className="header--text" onClick={toggleShowGallery}><img src={logo} alt='' style={{ width: '35px', height: '25px', paddingRight: '10px', marginTop: '5px'}}/> I G E L</h1>
-            <div className="my-info">
-                <h3 className="my-info-data" onClick={toggleShowGallery}> Gallery </h3> 
-                <h3 className="my-info-data" onClick={toggleShowAbout}> About </h3>
-                <h3 className="my-info-data" onClick={toggleShowContact}> Contact </h3>
+            <div className="subheader">
+                <h3 className="subheader-text" onClick={toggleShowGallery}> Gallery </h3> 
+                <h3 className="subheader-text" onClick={toggleShowAbout}> About </h3>
+                <h3 className="subheader-text" onClick={toggleShowContact}> Contact </h3>
             </div>
         </div>
     )
